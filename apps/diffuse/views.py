d10b3f076e66ea0c95e4a884236d48e0ba57c105
Andrew Burger <burger.andrewixl@gmail.com>
2017-11-11 16:51:23 -0800

    API and Home Page Added

--- apps/diffuse/views.py
+++ apps/diffuse/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+import urllib2
+import json
 
 def index(request):
    return render(request, 'diffuse/index.html')
+
+def view(request):
+	return render(request, 'diffuse/view.html')
 
 def home(request, game_id):
 	return render(request, 'diffuse/home.html')
@@ -10,4 +15,23 @@ def diffuse(request, game_id):
 	return render(request, 'diffuse/diffuse.html')
 
 def history(request, game_id):
-	return render(request, 'diffuse/history.html')
+    response = urllib2.urlopen('http://b5b1e790.ngrok.io/api/ActiveGame')
+    data = json.load(response)
+
+    jsondata = {}
+    for key, value in data.items():
+        jsondata[key] = value
+    context = {
+    'bombcode' : jsondata['code'],
+    'spies' : jsondata['spies'],
+    'players' : jsondata['players'],
+    'bombExplodeTime' : jsondata['bombExplodeTime'],
+    'badTeam' : jsondata['badTeam'],
+    'defuser' : jsondata['defuser'],
+    'goodTeam' : jsondata['goodTeam'],
+    'round' : jsondata['round'],
+    'gameID' : jsondata['gameID'],
+    'roundStatus' : jsondata['roundStatus'],
+    }
+    print jsondata
+    return render(request, 'diffuse/history.html', context)
