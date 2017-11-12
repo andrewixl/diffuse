5fe48041bfe184a308d950abddac40a5cdcc50c3
Andrew Burger <burger.andrewixl@gmail.com>
2017-11-12 03:51:50 -0800

    Stable Build

--- apps/diffuse/admin.py
+++ apps/diffuse/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Player, BombTime
+# Register your models here.
 
+admin.site.register(Player)
+admin.site.register(BombTime)
 # Register your models here.
